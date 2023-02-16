de9fe542138538735fd8a276e7c7a85ea392effb
Charlzzz <nts.sergei@yandex.ru>
2023-02-16 21:38:52 +0700

    13 homework parametrize

--- little_scripts/ln13.py
+++ little_scripts/ln13.py
@@ -34,9 +34,31 @@
 
 import json, pytest, requests
 class TestApi:
-    def test_agent(self):
+
+    user_agents = [
+        'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F)'
+         ' AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+        'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)'
+         ' CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+         ' Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'
+    ]
+
+
+    @pytest.mark.parametrize('user_agent', user_agents)
+    def test_agent(self, user_agent):
         url = 'https://playground.learnqa.ru/ajax/api/user_agent_check'
-        response = requests.get(url)
+        headers = {"User-Agent": user_agent}
+        exp_res1 = {
+            'platform': 'Unknown',
+            'browser': 'Unknown',
+            'device': 'Unknown',
+        }
+        response = requests.get(url, headers=headers)
         response_text = response.text
         response_json = json.loads(response_text)
-        print(response_json)
+
+        for key, value in exp_res1.items():
+            if response_json[key] == value:
+                print(f'{key} is {value} not valid in {headers}')
